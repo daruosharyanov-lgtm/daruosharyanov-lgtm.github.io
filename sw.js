b36481d085a219770d7971c48c06068864f92938
daruosharyanov-lgtm <daruosharyan.ov@gmail.com>
2026-06-06 16:20:35 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
+self.addEventListener("install", () => {
+  console.log("Da1u03h app installed");
+});
 
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
