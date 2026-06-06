5d0e863b19124cb5146aca6dda0072a5e23980e8
daruosharyanov-lgtm <daruosharyan.ov@gmail.com>
2026-06-06 16:41:04 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,14 +4,16 @@ const ASSETS = [
   '/index.html',
   '/Da1u03h_icon_192.png',
   '/Da1u03h_icon_512.png'
-]; // اینجا حتماً باید این پرانتز بسته باشه و کاماها رعایت شده باشن
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
