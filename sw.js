b1036c3c1cee315cbe9e12645ef71e217c0723a4
daruosharyanov-lgtm <daruosharyan.ov@gmail.com>
2026-06-06 16:35:13 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,17 @@
-self.addEventListener("install", () => {
-  console.log("Da1u03h app installed");
+const CACHE_NAME = 'da1u03h-site-v1';
+const ASSETS = [
+  '/',
+  '/index.html',// اگر فایل JS داری
+  '/Da1u03h_icon_192.png'
+  '/Da1u03h_icon_512.png'// آیکون‌ها رو هم کش کن
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
